6cf8cbef3ed51eb6809b575c6f4e7ffb938e4221
cat <cat.wallace0@gmail.com>
2025-03-23 16:37:01 +0000

    add binary sensor

---
+++ custom_components/smartthings/application_credentials.py
@@ -0,0 +1,6 @@
+"""Application credentials platform for SmartThings."""
+
+from homeassistant.components.smartthings import application_credentials
+
+class SmartThingsOAuth2Implementation(application_credentials.SmartThingsOAuth2Implementation):
+    pass
---
+++ custom_components/smartthings/binary_sensor.py
@@ -0,0 +1,6 @@
+"""Support for binary sensors through the SmartThings cloud API."""
+
+from homeassistant.components.smartthings import binary_sensor
+
+class SmartThingsBinarySensor(binary_sensor.SmartThingsBinarySensor):
+    pass
