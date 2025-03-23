d507b4f07fe966989a35a81c8740600bff964455
cat <cat.wallace0@gmail.com>
2025-03-23 13:59:52 +0000

    add config_flow

---
+++ custom_components/smartthings/config_flow.py
@@ -0,0 +1,6 @@
+"""Config flow to configure SmartThings."""
+
+from homeassistant.components.smartthings import config_flow
+
+class SmartThingsConfigFlow(config_flow.SmartThingsConfigFlow):
+    pass
