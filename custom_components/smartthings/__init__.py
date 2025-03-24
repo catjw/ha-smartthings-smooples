d0b16af7caabb0e2c48b0733b114d1112e797500
cat <cat.wallace0@gmail.com>
2025-03-24 20:57:22 +0000

    update init

--- custom_components/smartthings/__init__.py
+++ custom_components/smartthings/__init__.py
@@ -8,3 +8,8 @@ class SmartThingsData(smartthings.SmartThingsData):
 
 class FullDevice(smartthings.FullDevice):
     pass
+
+async def async_setup_entry(hass: smartthings.HomeAssistant, entry: smartthings.SmartThingsConfigEntry) -> bool:
+    """Initialize config entry which represents an installed SmartApp."""
+    return smartthings.async_setup_entry(hass, entry)
+    
