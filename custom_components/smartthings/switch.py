220ebb398fec2818b30b9459f716a80a8b5a9461
cat <cat.wallace0@gmail.com>
2025-04-07 07:41:46 +0100

    update

--- custom_components/smartthings/switch.py
+++ custom_components/smartthings/switch.py
@@ -41,6 +41,59 @@ MEDIA_PLAYER_CAPABILITIES = (
     Capability.MEDIA_PLAYBACK,
 )
 
+SWITCH = switch.SmartThingsSwitchEntityDescription(
+    key=Capability.SWITCH,
+    status_attribute=Attribute.SWITCH,
+    name=None,
+)
+
+CAPABILITY_TO_COMMAND_SWITCHES: dict[
+    Capability | str, switch.SmartThingsCommandSwitchEntityDescription
+] = {
+    Capability.CUSTOM_DRYER_WRINKLE_PREVENT: switch.SmartThingsCommandSwitchEntityDescription(
+        key=Capability.CUSTOM_DRYER_WRINKLE_PREVENT,
+        translation_key="wrinkle_prevent",
+        status_attribute=Attribute.DRYER_WRINKLE_PREVENT,
+        command=Command.SET_DRYER_WRINKLE_PREVENT,
+    )
+}
+CAPABILITY_TO_SWITCHES: dict[Capability | str, switch.SmartThingsSwitchEntityDescription] = {
+    Capability.SAMSUNG_CE_WASHER_BUBBLE_SOAK: switch.SmartThingsSwitchEntityDescription(
+        key=Capability.SAMSUNG_CE_WASHER_BUBBLE_SOAK,
+        translation_key="bubble_soak",
+        status_attribute=Attribute.STATUS,
+    ),
+    Capability.SWITCH: switch.SmartThingsSwitchEntityDescription(
+        key=Capability.SWITCH,
+        status_attribute=Attribute.SWITCH,
+        component_translation_key={
+            "icemaker": "ice_maker",
+        },
+    ),
+}
+
+
+@dataclass(frozen=True, kw_only=True)
+class SamsungOcfSwitchEntityDescription(SwitchEntityDescription):
+    """Describe a SmartThings switch entity."""
+
+    status_attribute: Attribute
+    component_translation_key: dict[str, str] | None = None
+
+@dataclass
+class SmartThingsExecuteCommands:
+    name: str
+    page: str
+    section: str
+    on: str
+    off: str
+    
+    @property
+    def set_off(self) -> list[str, dict[str, list[str]]]:
+        return [self.page, {self.section: [self.off]}]
+    @property
+    def set_on(self) -> list[str, dict[str, list[str]]]:
+        return [self.page, {self.section: [self.on]}]
 
 async def async_setup_entry(
     hass: HomeAssistant,
@@ -49,4 +102,78 @@ async def async_setup_entry(
 ) -> None:
     """Add switches for a config entry."""
     await switch.async_setup_entry(hass, entry, async_add_entities)
+    # entry_data = entry.runtime_data
+    # entities: list[SmartThingsEntity] = []
+    # for device in entry_data.devices.values():
+    #     media_player = all(
+    #             capability in device.status[MAIN]
+    #             for capability in switch.MEDIA_PLAYER_CAPABILITIES
+    #         )
+    #     appliance = (
+    #         device.device.components[MAIN].manufacturer_category
+    #         in INVALID_SWITCH_CATEGORIES
+    #     )
+    #     print('111111111111111111111111111111111111111111111111111111')
+    #     if (
+    #         device.device.type == "OCF"
+    #         and device.status[MAIN][Capability.OCF][Attribute.MANUFACTURER_NAME].value == "Samsung Electronics"
+    #     ):
+    #         print('222222222222222222222222222222222222222222222222222')
+    #         model = device.status[MAIN][Capability.OCF][Attribute.MODEL_NUMBER].value.split("|")[0]
+    #         print(model)
+    #         print('33333333333333333333333333333333333333333333333333333333333333333333')
+    #         if (
+    #             Capability.EXECUTE
+    #             and device.device.device_id == "climate"
+    #             and model not in (
+    #                 "SAC_SLIM1WAY",
+    #                 "SAC_BIG_SLIM1WAY",
+    #                 "MIM-H04EN",
+    #             )
+    #         ):
+    #             print('444444444444444444444444444444444444444444')
+    #             entities.extend(
+    #                 SamsungOcfSwitch(
+    #                     client=entry_data.client,
+    #                     device=device,
+    #                     entity_description=SamsungOcfSwitchEntityDescription(
+    #                         key=Capability.SWITCH,
+    #                         status_attribute=Attribute.SWITCH,
+    #                         translation_key="light",
+    #                         # component_translation_key={
+    #                         #     "icemaker": "ice_maker",
+    #                         # },    
+    #                     ),
+    #                     # capability=Capability.SWITCH,
+    #                     # component=MAIN,
+    #                     commands=SmartThingsExecuteCommands(
+    #                         'Light',
+    #                         'mode/vs/0', 
+    #                         'x.com.samsung.da.options',
+    #                         'Light_On',
+    #                         'Light_Off',
+    #                     ),
+    #                 )
+    #             )
+    # print('=========================================================')
+    # async_add_entities(entities)
+    # print('___________________________________________________')
     
+    
+class SamsungOcfSwitch(switch.SmartThingsSwitch, SmartThingsExecuteCommands):
+    """Representation of a Samsung OCF switch."""
+    def __init__(
+        self,
+        commands: SmartThingsExecuteCommands,
+    ) -> None:
+        """Initialize the switch."""
+        super().__init__()
+        self.commands = commands
+
+    async def async_turn_off(self, **kwargs: Any) -> None:
+        """Turn the switch off."""
+        await self.execute_device_command('execute', 'execute', self.commands.set_off)
+        
+    async def async_turn_on(self, **kwargs: Any) -> None:
+        """Turn the switch off."""
+        await self.execute_device_command('execute', 'execute', self.commands.set_on)
