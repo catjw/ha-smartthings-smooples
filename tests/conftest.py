0426efa472399854f9867d99fd6de7f475898512
cat <cat.wallace0@gmail.com>
2025-04-13 19:10:42 +0100

    update links

--- tests/conftest.py
+++ tests/conftest.py
@@ -1,9 +1,9 @@
-import json
-import logging
-import os
+"""Test configuration and mocks for the SmartThings component."""
+
+from collections.abc import Generator
 import time
-from typing import Generator
 from unittest.mock import AsyncMock, patch
+
 from pysmartthings import (
     DeviceResponse,
     DeviceStatus,
@@ -18,6 +18,15 @@ from homeassistant.components.application_credentials import (
     ClientCredential,
     async_import_client_credential,
 )
+from homeassistant.components.smartthings import CONF_INSTALLED_APP_ID
+from homeassistant.components.smartthings.const import (
+    CONF_LOCATION_ID,
+    CONF_REFRESH_TOKEN,
+    SCOPES,
+)
+from homeassistant.const import CONF_ACCESS_TOKEN, CONF_CLIENT_ID, CONF_CLIENT_SECRET
+from homeassistant.core import HomeAssistant
+from homeassistant.setup import async_setup_component
 
 from homeassistant.helpers import (
     area_registry as ar,
@@ -32,37 +41,14 @@ from homeassistant.helpers import (
     recorder as recorder_helper,
     translation as translation_helper,
 )
-from homeassistant.exceptions import HomeAssistantError
-from homeassistant.components.number import NumberEntity
-from homeassistant.components.select import SelectEntity
-from homeassistant.components.switch import SwitchEntity
-from homeassistant.setup import async_setup_component
-from homeassistant import config_entries
-from homeassistant.const import (
-    CONF_HOST,
-    CONF_EMAIL,
-    CONF_PASSWORD,
-    CONF_PROTOCOL,
-    CONF_SCAN_INTERVAL,
-    CONF_TIMEOUT, CONF_ACCESS_TOKEN, CONF_CLIENT_ID, CONF_CLIENT_SECRET
+from custom_components.smartthings import (
+    DOMAIN,
 )
-from homeassistant.core import HomeAssistant
-from homeassistant.util.unit_system import US_CUSTOMARY_SYSTEM, METRIC_SYSTEM
-
 
 from pytest_homeassistant_custom_component.common import MockConfigEntry, load_fixture
 from pytest_homeassistant_custom_component.syrupy import HomeAssistantSnapshotExtension
 from syrupy.assertion import SnapshotAssertion
 
-from custom_components.smartthings import (
-    DOMAIN,
-)
-from homeassistant.components.smartthings import CONF_INSTALLED_APP_ID
-from homeassistant.components.smartthings.const import (
-    CONF_LOCATION_ID,
-    CONF_REFRESH_TOKEN,
-    SCOPES,
-)
 pytest_plugins = "pytest_homeassistant_custom_component"
 
 
@@ -122,19 +108,22 @@ def label_registry(hass: HomeAssistant) -> lr.LabelRegistry:
     return lr.async_get(hass)
 
 
+
 @pytest.fixture
-def mock_setup_entry():
-    """Mock the setup entry."""
+def mock_setup_entry() -> Generator[AsyncMock]:
+    """Override async_setup_entry."""
     with patch(
         "custom_components.smartthings.async_setup_entry",
         return_value=True,
     ) as mock_setup_entry:
         yield mock_setup_entry
-        
+
+
 @pytest.fixture(name="expires_at")
 def mock_expires_at() -> int:
     """Fixture to set the oauth token expiration time."""
     return time.time() + 3600
+
 
 @pytest.fixture(autouse=True)
 async def setup_credentials(hass: HomeAssistant) -> None:
@@ -146,7 +135,8 @@ async def setup_credentials(hass: HomeAssistant) -> None:
         ClientCredential("CLIENT_ID", "CLIENT_SECRET"),
         DOMAIN,
     )
-        
+
+
 @pytest.fixture
 def mock_smartthings() -> Generator[AsyncMock]:
     """Mock a SmartThings client."""
@@ -157,7 +147,6 @@ def mock_smartthings() -> Generator[AsyncMock]:
         ) as mock_client,
         patch(
             "custom_components.smartthings.config_flow.config_flow.SmartThings",
-            # "homeassistant.components.smartthings.config_flow.SmartThings",
             new=mock_client,
         ),
     ):
@@ -175,7 +164,8 @@ def mock_smartthings() -> Generator[AsyncMock]:
             load_fixture("subscription.json", DOMAIN)
         )
         yield client
-        
+
+
 @pytest.fixture(
     params=[
         "da_ac_airsensor_01001",
@@ -233,15 +223,16 @@ def mock_smartthings() -> Generator[AsyncMock]:
         "ikea_kadrilj",
         "aux_ac",
         "hw_q80r_soundbar",
+        "gas_meter",
     ]
-)        
-
+)
 def device_fixture(
     mock_smartthings: AsyncMock, request: pytest.FixtureRequest
 ) -> Generator[str]:
     """Return every device."""
     return request.param
-        
+
+
 @pytest.fixture
 def devices(mock_smartthings: AsyncMock, device_fixture: str) -> Generator[AsyncMock]:
     """Return a specific device."""
@@ -252,6 +243,7 @@ def devices(mock_smartthings: AsyncMock, device_fixture: str) -> Generator[Async
         load_fixture(f"device_status/{device_fixture}.json", DOMAIN)
     ).components
     return mock_smartthings
+
 
 @pytest.fixture
 def mock_config_entry(expires_at: int) -> MockConfigEntry:
@@ -276,7 +268,8 @@ def mock_config_entry(expires_at: int) -> MockConfigEntry:
         version=3,
         minor_version=2,
     )
-    
+
+
 @pytest.fixture
 def mock_old_config_entry() -> MockConfigEntry:
     """Mock the old config entry."""
