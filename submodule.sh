799b70b262ca775951f2e160f37903194e40a21e
Cat <cat.wallace0@gmail.com>
2025-04-15 09:12:33 +0100

    update submodule

--- submodule.sh
+++ submodule.sh
@@ -1,6 +1,5 @@
 #! /usr/bin/env bash
 
-
 git submodule init core
 git clone --no-checkout --depth=1 $(git config submodule.core.url) core
 
@@ -8,6 +7,8 @@ cd core
 git sparse-checkout set \
 'tests/components/smartthings/fixtures' \
 'tests/components/smartthings/snapshots'
+
+git checkout tags/$(grep homeassistant== requirements.test.txt  | awk -F"=" '{print $3}')
 cd -
 
 git submodule absorbgitdirs core
