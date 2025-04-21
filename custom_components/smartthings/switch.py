f1c2da2c635b324d75ec0efb856def6f6877fa38
cat <cat.wallace0@gmail.com>
2025-04-21 15:44:30 +0100

    go again for display

--- custom_components/smartthings/switch.py
+++ custom_components/smartthings/switch.py
@@ -180,29 +180,35 @@ class SamsungOcfSwitch(switch.SmartThingsCommandSwitch, SmartThingsExecuteComman
     @property
     def is_on(self) -> bool:
         """Return true if the switch is on."""
-        _LOGGER.warning(f"***IS ON: {self._attr_is_on}")
-        if self.commands.page in self.get_attribute_data(self.switch_capability, self.entity_description.status_attribute)['href']:
-            if self._attr_is_on == None:
-                asyncio.create_task(self.async_turn_on())
-                self._attr_is_on = True
-            return self._attr_is_on
+        _LOGGER.warning(f"***IS ON 1: {self._attr_is_on}")
+        # if self.commands.page in self.get_attribute_data(self.switch_capability, self.entity_description.status_attribute)['href']:
+        if self._attr_is_on == None:
+            asyncio.create_task(self.async_turn_on())
+        _LOGGER.warning(f"***IS ON 2: {self._attr_is_on}")
+        self._attr_is_on = True
+        _LOGGER.warning(f"***IS ON 3: {self._attr_is_on}")
+        return self._attr_is_on
     
     async def async_turn_off(self, **kwargs: Any) -> None:
         """Turn the switch off."""
-        _LOGGER.warning(f"***TURN OFF: {self.state}")
+        _LOGGER.warning(f"***TURN OFF 1: {self.state}")
         await self.execute_device_command(
             self.switch_capability,
             self.entity_description.command,
             self.commands.set_off
         )
+        _LOGGER.warning(f"***TURN OFF 2: {self.state}")
         self._attr_is_on = False
+        _LOGGER.warning(f"***TURN OFF 3: {self.state}")
 
     async def async_turn_on(self, **kwargs: Any) -> None:
         """Turn the switch off."""
-        _LOGGER.warning(f"***TURN ON: {self.state}")
+        _LOGGER.warning(f"***TURN ON 1: {self.state}")
         await self.execute_device_command(
             self.switch_capability,
             self.entity_description.command,
             self.commands.set_on
         )
+        _LOGGER.warning(f"***TURN ON 2: {self.state}")
         self._attr_is_on = True
+        _LOGGER.warning(f"***TURN ON 3: {self.state}")
