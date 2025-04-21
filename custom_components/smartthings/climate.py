5bcb5a31d8426cde6d361c7bcee0e3e69d0a8d8c
cat <cat.wallace0@gmail.com>
2025-04-21 16:51:56 +0100

    update climate

--- custom_components/smartthings/climate.py
+++ custom_components/smartthings/climate.py
@@ -4,6 +4,7 @@ from homeassistant.core import HomeAssistant
 from homeassistant.helpers.entity_platform import AddConfigEntryEntitiesCallback
 
 from homeassistant.components.smartthings import climate
+from pysmartthings import Attribute, Capability
 
 from . import SmartThingsConfigEntry
 
@@ -14,4 +15,30 @@ async def async_setup_entry(
     async_add_entities: AddConfigEntryEntitiesCallback,
 ) -> None:
     """Add climate entities for a config entry."""
-    await climate.async_setup_entry(hass, entry, async_add_entities)
+    entry_data = entry.runtime_data
+    entities: list[climate.ClimateEntity] = [
+        SmartThingsRoomAirConditioner(entry_data.client, device)
+        for device in entry_data.devices.values()
+        if all(capability in device.status[climate.MAIN] for capability in climate.AC_CAPABILITIES)
+    ]
+    entities.extend(
+        climate.SmartThingsThermostat(entry_data.client, device)
+        for device in entry_data.devices.values()
+        if all(
+            capability in device.status[climate.MAIN] for capability in climate.THERMOSTAT_CAPABILITIES
+        )
+    )
+    async_add_entities(entities)
+
+
+class SmartThingsRoomAirConditioner(climate.SmartThingsAirConditioner):
+    def _determine_preset_modes(self) -> list[str] | None:
+        """Return a list of available preset modes."""
+        if self.supports_capability(Capability.CUSTOM_AIR_CONDITIONER_OPTIONAL_MODE):
+            supported_modes = self.get_attribute_value(
+                Capability.CUSTOM_AIR_CONDITIONER_OPTIONAL_MODE,
+                Attribute.SUPPORTED_AC_OPTIONAL_MODE,
+            )
+            if supported_modes :
+                return supported_modes
+        return None
