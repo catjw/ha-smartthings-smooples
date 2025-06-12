015fb73618f357b97de505e571f6e84ca6b5feca
Cat <cat.wallace0@gmail.com>
2025-06-12 09:06:01 +0100

    update for 2025.6.0

---
+++ custom_components/smartthings/water_heater.py
@@ -0,0 +1,18 @@
+"""Support for water heaters through the SmartThings cloud API."""
+
+from homeassistant.core import HomeAssistant
+from homeassistant.helpers.entity_platform import AddConfigEntryEntitiesCallback
+
+from homeassistant.components.smartthings import water_heater
+
+from . import SmartThingsConfigEntry
+
+
+async def async_setup_entry(
+    hass: HomeAssistant,
+    entry: SmartThingsConfigEntry,
+    async_add_entities: AddConfigEntryEntitiesCallback,
+) -> None:
+    """Add water heaters for a config entry."""
+    await water_heater.async_setup_entry(hass, entry, async_add_entities)
+
