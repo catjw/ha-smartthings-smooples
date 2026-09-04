c89eb4959694cf0bd6a168995f0659dc9c5b9df3
Cat <cat.wallace0@gmail.com>
2026-09-04 12:06:13 +0100

    fix tests

--- tests/test_diagnostics.py
+++ tests/test_diagnostics.py
@@ -65,7 +65,7 @@ async def test_device_diagnostics(
     await setup_integration(hass, mock_config_entry)
 
     device = device_registry.async_get_device_by_identifier(
-        identifiers={(DOMAIN, "96a5ef74-5832-a84b-f1f7-ca799957065d")}
+        (DOMAIN, "96a5ef74-5832-a84b-f1f7-ca799957065d"), mock_config_entry.entry_id
     )
 
     mock_smartthings.get_raw_device_status.reset_mock()
